45df36b37011532ea9226eadf0d9f4bcd69dd602
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-05-21 01:26:36 +0400

    added a way to read the contents of a file

--- repositories/users.js
+++ repositories/users.js
@@ -1,3 +1,6 @@
+//require the fs module
+const fs = require('fs')
+
 //create a class that'll allow us store user data
 class usersRepository {
     constructor(filename) {
@@ -7,8 +10,29 @@ class usersRepository {
 
         
         this.filename = filename;
+        //check to see if a file exists
+        try {
+            fs.accessSync(this.filename)
+        } catch (error) {
+            //create file it it doesn't exist
+            fs.writeFileSync(this.filename, '[]')
+
+        }
+    }
+
+    //implement methods for database usage
+    async getAll() {
+        //open the file called this.filename
+        const contents = await fs.promises.readFile(this.filename, { encoding: 'utf8'})
+
+        //Read the contents of the file
+        fsPromise = fs.open(this.filename)
+        //Parse the contents
+
+        //Return the parsed data 
+
     }
 
 }
 
-new usersRepository()
+const repo = new usersRepository('users.json')
