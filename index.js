d2e7228d151b5a9c50e3c0111c6d14899c6f8caf
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-05-24 01:10:07 +0400

    added a way to log & added error handling

--- index.js
+++ index.js
@@ -32,10 +32,6 @@ app.get('/signup', (req, res) => {
     `)
 })
 
-app.get('/login', (req, res) => {
-
-})
-
 
 app.post('/signup', async (req, res) => {
     //do the signup validation logic 
@@ -63,9 +59,42 @@ app.post('/signup', async (req, res) => {
 
 app.get('/signout', (req, res) => {
     req.session = null
-    req.send('You are now logged out.')
+    res.send('You are now logged out.')
 })
 
+app.get('/login', (req, res) => {
+    res.send(`
+    <div>
+    You're now signed in and your ID is ${req.session.userId}.
+    <form method="POST">
+        <input name="email" placeholder="Your email" />
+        <input name="password" placeholder="A password" />
+        <button>Log in</button>
+    </form>
+</div>
+    `)
+})
+
+app.post('/login', async (req, res) => {
+    const { email, password} = req.body
+
+    const savedUser = await usersRepo.getOneBy({ email })
+    if (!savedUser) {
+        return res.send(`Error: Email not found.`)
+    }
+
+    //check if passwords match 
+    if (savedUser.password !== password) {
+        return res.send(`Error: Invalid password.`)
+    }
+
+    //use cookie-based auth in our app by creating a user to represent each person
+    const user = await usersRepo.create({ email, password })
+    //store the user's id inside the users cookie via cookie-session
+    req.session.userId = user.id
+
+    res.send("You're signed in successfully!")
+})
 
 //have the application listen to requests
 app.listen(port, () => {
