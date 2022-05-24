5b438814ceae73356229c6de3536a37c28f664f2
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-05-25 01:20:46 +0400

    added a layout file for HTML templating

--- views/admin/auth/login.js
+++ views/admin/auth/login.js
@@ -1,12 +1,16 @@
+//require out layout for creating reusable content/templates
+const layout = require('../layout');
+
 module.exports = ({ req }) => { 
-    return `
-        <div>
+    return layout({ 
+    content: `
+    <div>
         You're now logged in and your ID is ${req.session.userId}.
         <form method="POST">
             <input name="email" placeholder="Your email" />
             <input name="password" placeholder="A password" />
             <button>Log in</button>
         </form>
-    </div>
-    `
+    </div>  
+    `})
 }
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,14 +1,17 @@
+const layout = require('../layout');
+
 //create a function that handles HTML template creation
 module.exports = ({ req }) => {
-    return  `
-    <div>
-    Your ID is ${req.session.userId}
-    <form method="POST">
-        <input name="email" placeholder="Your email" />
-        <input name="password" placeholder="A password" />
-        <input name="confirmPassword" placeholder="Confirm password" />
-        <button>Sign up</button>
-    </form>
-    </div>
-    `
+    return layout({ 
+        content: `
+        <div>
+            Your ID is ${req.session.userId}
+            <form method="POST">
+                <input name="email" placeholder="Your email" />
+                <input name="password" placeholder="A password" />
+                <input name="confirmPassword" placeholder="Confirm password" />
+                <button>Sign up</button>
+            </form>
+        </div>
+    `})
 }
