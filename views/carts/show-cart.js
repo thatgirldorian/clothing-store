d936c836fc4c0c7277e5e543540430c1b9504c62
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-06-01 23:24:24 +0400

    added a way to remove items from the cart

--- views/carts/show-cart.js
+++ views/carts/show-cart.js
@@ -20,12 +20,13 @@ module.exports = ({ items }) => {
                 $${item.product.price * item.quantity}
             </div>
             <div class="remove">
-                <form method="POST">
-                <button class="button is-danger">                  
-                    <span class="icon is-small">
-                    <i class="fas fa-times"></i>
-                    </span>
-                </button>
+                <form method="POST" action="/cart/products/delete">
+                    <input hidden value="${item.id}" name="itemId">
+                    <button class="button is-danger">                  
+                        <span class="icon is-small">
+                        <i class="fas fa-times"></i>
+                        </span>
+                    </button>
                 </form>
             </div>
             </div>
